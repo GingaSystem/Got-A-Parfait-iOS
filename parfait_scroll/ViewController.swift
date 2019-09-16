b8a1857547ef3ec5239ec632277541a0a070f306
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2019-09-16 15:24:06 +0900

    update

--- parfait_scroll/ViewController.swift
+++ parfait_scroll/ViewController.swift
@@ -9,27 +9,14 @@
 import UIKit
 class ViewController: UIViewController{
     
-    
-    @IBAction func dragging(_ sender: UIPanGestureRecognizer) {
-        
-    }
     @IBOutlet weak var scroll: UIScrollView!
-    
-    @IBOutlet weak var scroll2: UIScrollView!
+    @IBOutlet weak var scrollContentView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let scrollContentView = createContentView()
-        scroll.addSubview(scrollContentView)
-        scrollContentView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            scrollContentView.heightAnchor.constraint(equalTo: scroll.heightAnchor),
-            scrollContentView.widthAnchor.constraint(equalTo: scroll.widthAnchor, multiplier: 5),
-            scrollContentView.topAnchor.constraint(equalTo: scroll.topAnchor),
-            scrollContentView.bottomAnchor.constraint(equalTo: scroll.bottomAnchor),
-            ])
-        scroll.contentSize = scroll.frame.size
+        // パーツ選択スクロールのセットアップ
+        setupScrollContentView()
         scroll.isPagingEnabled = true
     }
     
@@ -53,109 +40,86 @@ class ViewController: UIViewController{
         
     }
     
-    func createContentView() -> UIView {
-        let contentView = UIView()
-        contentView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-        return contentView
+    @IBAction func dragging(_ sender: UIPanGestureRecognizer) {
         
+    }
+    
+    func setupScrollContentView() {
+        let contentView = scrollContentView!
         let stack = UIStackView()
-        
         contentView.addSubview(stack)
         stack.axis = .horizontal
         stack.distribution = .fillEqually
         stack.translatesAutoresizingMaskIntoConstraints = false
-        stack.heightAnchor.constraint(equalTo: contentView.heightAnchor).isActive = true
-        stack.widthAnchor.constraint(equalTo: contentView.widthAnchor).isActive = true
-        stack.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
-        stack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
-
+        NSLayoutConstraint.activate([
+            stack.heightAnchor.constraint(equalTo: contentView.heightAnchor),
+            stack.widthAnchor.constraint(equalTo: contentView.widthAnchor),
+            stack.topAnchor.constraint(equalTo: contentView.topAnchor),
+            stack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            ])
         
         let allParts = [ParfaitPart.getToppings(),
                         ParfaitPart.getTopIces(),
                         ParfaitPart.getFruits(),
                         ParfaitPart.getBottomIces(),
                         ParfaitPart.getSyrups()]
-        for i in 0...4{
-            
-            let pageView = createPage(parfaitParts: allParts[i])
+        allParts.forEach {
+            let pageView = createPage(parts: $0)
             stack.addArrangedSubview(pageView)
-            //pageView.translatesAutoresizingMaskIntoConstraints = false
-            //pageView.widthAnchor.constraint(equalTo: contentView.widthAnchor).isActive = true
-            //pageView.heightAnchor.constraint(equalTo: contentView.heightAnchor).isActive = true
-            
-            
-            
+            pageView.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                pageView.widthAnchor.constraint(equalTo: scroll.widthAnchor),
+                pageView.heightAnchor.constraint(equalTo: scroll.heightAnchor),
+                ])
         }
-        return contentView
-        
+        NSLayoutConstraint.activate([
+            contentView.widthAnchor.constraint(equalTo: scroll.widthAnchor, multiplier: CGFloat(allParts.count))
+            ])
     }
-    func createPage(parfaitParts : [ParfaitPart])->UIView{
+    
+    func createPage(parts : [ParfaitPart])->UIView{
         let pageView = UIView()
         
-        
-        
-        
-        
-        
-        //ここからスタック追加コード
         let stackV:UIStackView = UIStackView()
         pageView.addSubview(stackV)
-        stackV.backgroundColor = UIColor.cyan
         stackV.axis = .vertical
-        stackV.translatesAutoresizingMaskIntoConstraints = false
-        stackV.widthAnchor.constraint(equalTo: pageView.widthAnchor).isActive = true
-        stackV.heightAnchor.constraint(equalTo: pageView.heightAnchor).isActive = true
         stackV.distribution = .fillEqually
+        stackV.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackV.widthAnchor.constraint(equalTo: pageView.widthAnchor),
+            stackV.heightAnchor.constraint(equalTo: pageView.heightAnchor),
+            ])
         
         let stackH1:UIStackView = UIStackView()
         let stackH2:UIStackView = UIStackView()
-        stackV.addArrangedSubview(stackH1)
-        stackV.addArrangedSubview(stackH2)
+        func setupStackH(_ stack: UIStackView) {
+            stackV.addArrangedSubview(stack)
+            stack.axis = .horizontal
+            stack.frame = CGRect(x: 0, y: 0, width: stackV.frame.width, height: stackV.frame.height / 2)
+            stack.distribution = .fillEqually
+        }
+        setupStackH(stackH1)
+        setupStackH(stackH2)
         
-        stackH1.backgroundColor = UIColor.cyan
-        stackH1.axis = .horizontal
-        stackH1.frame = CGRect(x: 0, y: 0, width: stackV.frame.width, height: stackV.frame.height / 2)
-        stackH1.distribution = .fillEqually
-        stackH2.backgroundColor = UIColor.cyan
-        stackH2.axis = .horizontal
-        stackH2.frame = CGRect(x: 0, y: 0, width: stackV.frame.width, height: stackV.frame.height / 2)
-        stackH2.distribution = .fillEqually
-        
-        
-        
-        for i in 0...2 {
-            let img = UIImageView(image: parfaitParts[i].partImage)
+        func createPartsImageView(_ image: UIImage) -> UIImageView {
+            let img = UIImageView(image: image)
             let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.imageTapped(_:)))
+            img.contentMode = .scaleAspectFit
             img.isUserInteractionEnabled = true
             img.addGestureRecognizer(tapGesture)
-            stackH1.addArrangedSubview(img)
+            return img
         }
         
-        for i in 3...4 {
-            let img = UIImageView(image: parfaitParts[i].partImage)
-            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.imageTapped(_:)))
-            img.isUserInteractionEnabled = true
-            img.addGestureRecognizer(tapGesture)
-            stackH2.addArrangedSubview(img)
+        let firstRowCount = parts.count / 2 + parts.count % 2
+        for i in 0..<firstRowCount {
+            stackH1.addArrangedSubview(createPartsImageView(parts[i].partImage))
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        //ここまで
-        
-        
+        for i in firstRowCount..<parts.count {
+            stackH2.addArrangedSubview(createPartsImageView(parts[i].partImage))
+        }
         
         return pageView
         
     }
-    
-    
-    
 }
 
