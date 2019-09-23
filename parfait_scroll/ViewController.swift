58cec55e0ecb3033be65a6a0c2d9295837de7321
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2019-09-23 23:56:44 +0900

    fix picker scroll margin

--- parfait_scroll/ViewController.swift
+++ parfait_scroll/ViewController.swift
@@ -112,7 +112,7 @@ class ViewController: UIViewController {
         setupStackH(stackH1)
         setupStackH(stackH2)
         
-        func createPartsImageView(_ part: ParfaitPart) -> UIImageView {
+        func createPartsImageView(_ stack: UIStackView, _ part: ParfaitPart) {
             let img = UIImageView(image: part.image)
             img.isOpaque = false
             img.backgroundColor = .clear
@@ -123,15 +123,19 @@ class ViewController: UIViewController {
             interaction.isEnabled = true
             img.addInteraction(interaction)
             img.isUserInteractionEnabled = true
-            return img
+            
+            stack.addArrangedSubview(img)
+            NSLayoutConstraint.activate([
+                img.heightAnchor.constraint(equalTo: stack.heightAnchor, multiplier: 0.9),
+                ])
         }
         
         let firstRowCount = parts.count / 2 + parts.count % 2
         for i in 0..<firstRowCount {
-            stackH1.addArrangedSubview(createPartsImageView(parts[i]))
+            createPartsImageView(stackH1, parts[i])
         }
         for i in firstRowCount..<parts.count {
-            stackH2.addArrangedSubview(createPartsImageView(parts[i]))
+            createPartsImageView(stackH2, parts[i])
         }
         
         return pageView
